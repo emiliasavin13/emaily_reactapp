325b66a71c5fef828d123e54b2c2268aed32bfda
Emilia Savin <emilia.savin@appreciategroup.co.uk>
2020-03-27 15:09:00 +0000

    Authentication with Google OAuth Phase 1

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+/**Begin imports */
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+/**End imports */
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ buy: "buddy" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken: " + accessToken);
+      console.log("refreshToken: " + refreshToken);
+      console.log("profile: " + JSON.stringify(profile));
+      console.log("done: " + done);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
